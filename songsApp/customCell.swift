543601359681e180cb5ecd5f3779cd3f8a219daf
Ahmed Farido <AhmedFarido@github.com>
2018-03-04 00:32:26 +0200

    first commit

---
+++ songsApp/customCell.swift
@@ -0,0 +1,9 @@
+//
+//  customCell.swift
+//  songsApp
+//
+//  Created by FARIDO on 3/3/18.
+//  Copyright © 2018 FARIDO. All rights reserved.
+//
+
+import Foundation
---
+++ songsApp/videosCell.swift
@@ -0,0 +1,24 @@
+//
+//  videosCell.swift
+//  songsApp
+//
+//  Created by FARIDO on 3/3/18.
+//  Copyright © 2018 FARIDO. All rights reserved.
+//
+
+import UIKit
+
+class videosCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
